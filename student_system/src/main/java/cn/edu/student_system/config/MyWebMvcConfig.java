ce74a7b58c07a1dc34485fd0d1657b4130f0554d
LinIOT <105902872+LinIOT@users.noreply.github.com>
2022-05-25 19:12:06 +0800

    Delete student_system directory

--- student_system/src/main/java/cn/edu/student_system/config/MyWebMvcConfig.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.student_system.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-
-@Configuration
-public class MyWebMvcConfig {
-
-}
